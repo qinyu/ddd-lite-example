61d78a1f206aacfda2f086f8c683a484f4b2704f
qinyu <qinyusuain@gmail.com>
2021-11-20 11:37:21 +0800

    move old monolith to legacy, prepared for refactor

---
+++ legacy/admin/src/main/java/com/example/admin/service/GroupRequestManagementApplicationService.java
@@ -0,0 +1,14 @@
+package com.example.admin.service;
+
+import com.example.domain.group.service.GroupRequestService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GroupRequestManagementApplicationService {
+
+    private final GroupRequestService groupRequestService;
+
+    public GroupRequestManagementApplicationService(GroupRequestService groupRequestService) {
+        this.groupRequestService = groupRequestService;
+    }
+}
---
+++ legacy/business/src/main/java/com/example/business/usecase/question/CreateAnswerCase.java
@@ -0,0 +1,37 @@
+package com.example.business.usecase.question;
+
+import com.example.domain.question.model.Answer;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+
+public class CreateAnswerCase {
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Request {
+        @NotBlank(message = "answer_content_required")
+        private String content;
+    }
+
+    @Getter
+    @Setter
+    @Builder
+    public static class Response {
+        private String id;
+        private String content;
+
+        public static Response from(Answer answer) {
+            return Response.builder()
+                    .id(answer.getId())
+                    .content(answer.getContent())
+                    .build();
+        }
+    }
+}
